1ea2f6ebd1757dc0ea405960619f6f03608e6dcc
Francesco Colombo <francesco.colombo@admaiorastudio.com>
2016-12-29 15:06:55 +0100

    iOS: from Outlets -> to Widgets

--- Bugghy/Bugghy.iOS/Resources/Layouts/Cells/GimmickViewCell.designer.cs
+++ Bugghy/Bugghy.iOS/Resources/Layouts/Cells/GimmickViewCell.designer.cs
@@ -12,63 +12,9 @@ namespace AdMaiora.Bugghy
 	[Register ("GimmickViewCell")]
 	partial class GimmickViewCell
 	{
-		[Outlet]
-		public UIKit.UIView CalloutLayout { get; set; }
-
-		[Outlet]
-        public UIKit.UIView ContentLayout { get; set; }
-
-		[Outlet]
-        public UIKit.UIView DetailsLayout { get; set; }
-
-		[Outlet]
-        public UIKit.UILabel NameLabel { get; set; }
-
-		[Outlet]
-        public UIKit.UILabel OfLabel { get; set; }
-
-		[Outlet]
-        public UIKit.UILabel OwnerLabel { get; set; }
-
-		[Outlet]
-        public UIKit.UIImageView ThumbImage { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
-			if (CalloutLayout != null) {
-				CalloutLayout.Dispose ();
-				CalloutLayout = null;
-			}
-
-			if (ContentLayout != null) {
-				ContentLayout.Dispose ();
-				ContentLayout = null;
-			}
-
-			if (ThumbImage != null) {
-				ThumbImage.Dispose ();
-				ThumbImage = null;
-			}
-
-			if (DetailsLayout != null) {
-				DetailsLayout.Dispose ();
-				DetailsLayout = null;
-			}
-
-			if (NameLabel != null) {
-				NameLabel.Dispose ();
-				NameLabel = null;
-			}
-
-			if (OfLabel != null) {
-				OfLabel.Dispose ();
-				OfLabel = null;
-			}
-
-			if (OwnerLabel != null) {
-				OwnerLabel.Dispose ();
-				OwnerLabel = null;
-			}
 		}
 	}
 }
